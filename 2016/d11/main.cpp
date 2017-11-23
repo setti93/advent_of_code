8758f59b9fa832d148ed45c72fb489fa350dca9d
Alessandro Marzialetti <setti93@gmail.com>
2017-11-23 14:42:59 +0100

    not liking it

--- 2016/d11/main.cpp
+++ 2016/d11/main.cpp
@@ -1,24 +1,23 @@
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
-std::unordered_map<std::string, int> elems;
-std::vector<std::unordered_set<int>> floors;
-
-void read(const std::string &filename) {
-  ifstream f(filename);
-  if (f::fail())
+int read(const std::string &filename, std::vector<std::set<int>> &floors) {
+  int tot = 0;
+  std::ifstream f(filename);
+  if (f.fail())
     exit(1);
   std::string line;
-  int count = 1;
+  int count = 10;
+  std::unordered_map<std::string, int> elems;
   while (std::getline(f, line)) {
-    floors.push_back();
+    floors.emplace_back();
     auto &floor = floors.back();
     size_t pos = 0;
-    while ((pos = line.find(" a ", pos)) != line::npos) {
+    while ((pos = line.find(" a ", pos)) != std::string::npos) {
       pos += 3;
       size_t end = line.find_first_of(",.", pos);
       std::string item = line.substr(pos, end - (pos));
@@ -26,20 +25,56 @@ void read(const std::string &filename) {
       std::string type = item.substr((item.find_first_of("- ") + 1));
       int id;
       if (elems.find(element) == elems.end()) {
-        elems[element] = count++;
+        elems[element] = count;
+        count += 10;
       }
       id = elems[element];
-      if (type.find("generator") != type::npos)
-        id += 100;
+      if (type.find("generator") == std::string::npos)
+        id += 5;
       floor.emplace(id);
+      ++tot;
     }
   }
+  return tot;
+}
+
+enum class Go { Up, Down };
+
+bool try_move(std::vector<std::set<int>> &floors, int &e, Go direction,
+              int amount) {
+    
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2)
+  std::string filename;
+  if (argc == 1)
+    filename = "input.txt";
+  if (argc > 2)
     return 1;
-  read(argv[1]);
-
+  filename = std::string(argv[1]);
+  std::vector<std::set<int>> floors;
+  auto total = read(filename, floors);
+  for (auto &floor : floors) {
+    std::cout << "new f:\n";
+    for (auto &item : floor) {
+      if (item % 10 == 0)
+        std::cout << "  " << item / 10 << "G\n";
+      else
+        std::cout << "  " << item / 10 << "M\n";
+    }
+  }
+  int e = 0;
+  while (floors.back().size() != total) {
+    if (try_move(floors, e, Go::Up, 2))
+      ;
+    else if (try_move(floors, e, Go::Up, 1))
+      ;
+    else if (try_move(floors, e, Go::Down, 1))
+      ;
+    else if (try_move(floors, e, Go::Down, 2))
+      ;
+    else
+      break;
+  }
   return 0;
 }
